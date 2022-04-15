42d443aa44206cab804bb1e2e4ab60663f89ab97
PseudoDruid <70019140+PseudoDruid@users.noreply.github.com>
2022-04-15 15:39:16 -0400

    Added simple functionality to the Account page

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {useState} from "react";
 import { View, Button, Text, Animated, TextInput, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -190,37 +191,147 @@ function Home({ navigation }) {
 }
 
 function Account({ navigation }) {
+    const [editParent, setEditParent] = useState(false);
+    const [editChild, setEditChild] = useState(false);
+    const [editVehicle, setEditVehicle] = useState(false);
     return (
-        <View style={{ flex: 1, backgroundColor: '#FABD27' }}>
-            <View style={{ position: 'absolute' }}><Text style={styles.text}> Account </Text></View>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#FABD27' }}>
+            <Text style={styles.registerTitle}> Account </Text>
 
-            <View style={{marginBottom: 150}}></View>
+            <Text style={styles.registerTitle}>Parent's Info</Text>
 
-            <Text style={styles.fieldTitle}>First Name:</Text>
-    
-            <Text style={styles.fieldTitle}>Last Name:</Text>
+            <View style={{
+                backgroundColor: '#091638',
+                padding: 0,
+                width: "50%",
+                marginBottom: 0,
+                marginTop: 40,
+                borderRadius: 50,
+            }}><Text style={{
+                // text style for button.
+                fontSize: 18,
+                textAlign: 'center',
+                margin: 15,
+                color: 'white',
+            }} onPress={() => setEditParent(previousState => !previousState)}>Edit Parent's Info</Text></View>
 
-            <Text style={styles.fieldTitle}>Email Address:</Text>
+            <Text style={styles.fieldTitle}>First Name</Text>
+            <TextInput style={styles.input}
+                maxLength={25}
+                placeholder="First Name"
+                editable = {editParent} />
 
-            <Text style={styles.fieldTitle}>Phone Number:</Text>
+            <Text style={styles.fieldTitle}>Last Name</Text>
+            <TextInput style={styles.input}
+                maxLength={25}
+                placeholder="Last Name"
+                editable = {editParent} />
 
-            <Text style={styles.fieldTitle}>First Name:</Text>
+            <Text style={styles.fieldTitle}>Email Address</Text>
+            <TextInput style={styles.input}
+                maxLength={25}
+                placeholder="Email Address"
+                editable = {editParent} />
 
-            <Text style={styles.fieldTitle}>Last Name:</Text>
+            <Text style={styles.fieldTitle}>Phone Number</Text>
+            <TextInput style={styles.input}
+                maxLength={10}
+                keyboardType='numeric'
+                placeholder="Phone Number"
+                editable = {editParent} />
 
-            <Text style={styles.fieldTitle}>Date of Birth:</Text>
+            <Text style={styles.registerTitle}>Child's Info</Text>
 
-            <Text style={styles.fieldTitle}>Allergies:</Text>
+            <View style={{
+                backgroundColor: '#091638',
+                padding: 0,
+                width: "50%",
+                marginBottom: 0,
+                marginTop: 40,
+                borderRadius: 50,
+            }}><Text style={{
+                // text style for button.
+                fontSize: 18,
+                textAlign: 'center',
+                margin: 15,
+                color: 'white',
+            }} onPress={() => setEditChild(previousState => !previousState)}>Edit Child's Info</Text></View>
 
-            <Text style={styles.fieldTitle}>Additional Info:</Text>
+            <Text style={styles.fieldTitle}>First Name</Text>
+            <TextInput style={styles.input}
+                maxLength={25}
+                placeholder="First Name"
+                editable = {editChild} />
 
-            <Text style={styles.fieldTitle}>Model:</Text>
+            <Text style={styles.fieldTitle}>Last Name</Text>
+            <TextInput style={styles.input}
+                maxLength={25}
+                placeholder="Last Name"
+                editable = {editChild} />
 
-            <Text style={styles.fieldTitle}>Year:</Text>
+            <Text style={styles.fieldTitle}>Date of Birth</Text>
+            <TextInput style={styles.input}
+                maxLength={25}
+                keyboardType='numeric'
+                placeholder="Date of Birth"
+                editable = {editChild} />
 
-            <Text style={styles.fieldTitle}>Color:</Text>
+            <Text style={styles.fieldTitle}>Allergies</Text>
+            <TextInput style={styles.input}
+                maxLength={1000}
+                multiline={true}
+                placeholder="Allergies"
+                editable = {editChild} />
 
-            <Text style={styles.fieldTitle}>Plate Number:</Text>
+            <Text style={styles.fieldTitle}>Additional Info</Text>
+            <TextInput style={styles.input}
+                maxLength={25} // THIS SHOULD MATCH SETUP PAGE
+                multiline={true}
+                placeholder="Additional info to tell emergency services about this child"
+                editable = {editChild} />
+
+            <Text style={styles.registerTitle}>Vehicle Info</Text>
+
+            <View style={{
+                backgroundColor: '#091638',
+                padding: 0,
+                width: "50%",
+                marginBottom: 0,
+                marginTop: 40,
+                borderRadius: 50,
+            }}><Text style={{
+                // text style for button.
+                fontSize: 18,
+                textAlign: 'center',
+                margin: 15,
+                color: 'white',
+            }} onPress={() => setEditVehicle(previousState => !previousState)}>Edit Vehicle's Info</Text></View>
+
+            <Text style={styles.fieldTitle}>Model</Text>
+            <TextInput style={styles.input}
+                maxLength={25}
+                placeholder="Model"
+                editable = {editVehicle} />
+
+            <Text style={styles.fieldTitle}>Year</Text>
+            <TextInput style={styles.input}
+                maxLength={4}
+                keyboardType='numeric'
+                placeholder="Year"
+                editable = {editVehicle} />
+
+            <Text style={styles.fieldTitle}>Color</Text>
+            <TextInput style={styles.input}
+                maxLength={25}
+                placeholder="Color"
+                editable = {editVehicle} />
+
+            <Text style={styles.fieldTitle}>Plate Number</Text>
+            <TextInput style={styles.input}
+                maxLength={6}
+                keyboardType='numeric'
+                placeholder="Plate Number"
+                editable = {editVehicle} />
 
             <View style={styles.navBar}>
                 <Icon name="arrowright" size={25} color="white" type="antdesign" />
